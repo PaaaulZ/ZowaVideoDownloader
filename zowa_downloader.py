871b6c653b2af19c5abcab6ceaac2cba3fb8c3c6
PaaaulZ <46759927+PaaaulZ@users.noreply.github.com>
2021-11-03 19:21:57 +0100

    Added resolution check and fixed path error

--- zowa_downloader.py
+++ zowa_downloader.py
@@ -7,8 +7,9 @@ import ffmpeg
 
 BASE_API_URL = "https://api.zowa.app/api/v2/videos/pwa/"
 VIDEO_URL_REGEX = r"https://zowa.app/play/([0-9]{1,})"
+VIDEO_QUALITY_REGEX = r"-([0-9]{3,4})p.m3u8"
 
-def download_video(url):
+def download_video(url, output_path):
 
     print(f"[+] URL: {url}")
 
@@ -26,15 +27,29 @@ def download_video(url):
         json_data = json.loads(r.content)
         m3u8_url = json_data.get('video_url')
         if m3u8_url is not None:
-            print(f"\t[+] Final m3u8 link: {m3u8_url}\n\n")
+            print(f"\t[+] Found m3u8 link: {m3u8_url}\n")
+            r_m3u8 = requests.get(m3u8_url)
+            if r_m3u8.status_code == 200:
 
-            process = (
-                ffmpeg
-                .input(m3u8_url)
-                .filter('bsf', 'a aac_adtstoasc')
-                .output(f"{video_id}.mp4", codec='copy', vcodec='copy', crf=50)
-                .run()
-            )
+                available_resolutions = re.findall(VIDEO_QUALITY_REGEX, str(r_m3u8.content))
+                if len(available_resolutions) >= 1:
+                    # Search for the best resolution
+                    best_resolution = 0
+                    for resolution in available_resolutions:
+                        if int(resolution) > best_resolution:
+                            best_resolution = int(resolution)
+                    m3u8_url = m3u8_url.replace('.m3u8', f'-{resolution}p.m3u8')
+
+                print(f"\t[+] Final m3u8 link: {m3u8_url}\n\n")
+
+                process = (
+                    ffmpeg
+                    .input(m3u8_url)
+                    .output(f"{output_path}/{video_id}.mp4", codec='copy', vcodec='copy', crf=50)
+                    .run()
+                )
+            else:
+                print(f"\t[-] Unable to fetch .m3u8 stream")
         else:
             print("\t[-] Unable to find .m3u8 link")
         
@@ -49,6 +64,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Download videos from zowa.app')
     parser.add_argument('--url', '-u', action='store', type=str, help='URL of the video you want to download', required=True)
+    parser.add_argument('--output', '-o', action='store', type=str, help='Output path', required=True)
     args = parser.parse_args()
 
-    download_video(args.url)
+    download_video(args.url, args.output)
